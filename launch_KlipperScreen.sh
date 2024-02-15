7879eef5d5f228d9af6185a150e85aa1ffcfa8f4
GiacomoGuaresi <36533859+GiacomoGuaresi@users.noreply.github.com>
2024-02-15 15:13:07 +0100

    🔥 delete useless file

--- launch_KlipperScreen.sh
+++
@@ -1,5 +0,0 @@
-# Launch Updater
-/usr/bin/xinit /home/pi/.KlipperScreen-env/bin/python /home/pi/GingerUpdater/screen.py
-
-# Luanch KlipperScreen
-/usr/bin/xinit /home/pi/.KlipperScreen-env/bin/python /home/pi/KlipperScreen/screen.py
